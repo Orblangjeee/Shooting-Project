684a2cdefbeff31b5fe415344c493258b1de407f
Orblangjeee <wjddns0130@gmail.com>
2023-06-15 17:39:09 +0900

    0615

--- Scripts/Enemy.cs
+++ Scripts/Enemy.cs
@@ -1,7 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
 using UnityEditor.PackageManager.Requests;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
 
 //역할 : Enemy는 아래 방향으로 일정한 속도로 이동한다.
 // 방향 (아래 vector.down)
@@ -10,45 +13,82 @@ using UnityEngine;
 
 //역할 : Enemy와 Player가 충돌시 둘 다 Destroy
 
+//역할 : Enemy가 처음 생성될 때, 이동방식을 랜덤하게 지정
+// 1. 아래로 이동 2. 타깃 이동 3. 랜덤 확률 50%
+
 public class Enemy : MonoBehaviour
 {
-    public float speed = 10f;
-    public int enemyHp = 5;
+   
+
+    GameObject player; //타깃(Player)
+    Vector3 direction; //이동 방향
+
+    public float speed = 5f;
+    public int enemyHp = 3;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.Find("Player"); //plyer 게임 오브젝트 찾기
+
+        //랜덤 : 100개의 숫자 중 1개 뽑기
+        int randomNumber = Random.Range(0, 100);
+        //뽑은 숫자 50보다 큰 경우 : 아래 이동 / 작은 경우 : 타겟 이동
+        if (randomNumber >= 50)
+        {
+            direction = Vector3.down;
+        }
+        if (randomNumber < 50)
+        {
+            direction = player.transform.position - transform.position;
+            direction.Normalize();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += Vector3.down * speed * Time.deltaTime;
-       
+       // transform.position += Vector3.down * speed * Time.deltaTime;
+
+        transform.position += direction * speed * Time.deltaTime;
+
+        //player 적당히 따라가기
+        /*if (transform.position.y >= player.transform.position.y)
+        {
+            //player(타깃) 방향으로 일정한 속도로 이동한다.
+            //1. 어떤 방향으로 갈지 알아야 한다.
+            //Player를 바라보는 방향(Player 위치 - Enemy 위치)
+            direction = player.transform.position - transform.position;
+            //1-1. 방향 벡터의 크기를 1로 맞춘다. (속도 일정하게)
+            direction.Normalize();
+            //2. 해당 방향으로 일정한 속력으로 이동한다.
+        }*/
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-       
-    }
+
 
     private void OnCollisionEnter(Collision collision)
     {
         //물체 이름이 Missile 일 경우 파괴
         if (collision.gameObject.name.Contains("Missile"))
         {
+
             Destroy(collision.gameObject);
             enemyHp -= 1;
             Debug.Log("enemyHP:" + enemyHp);
             if (enemyHp <= 0)
             {
                 Destroy(gameObject);
+                // 0-3. ScoreManager.cs 컴포넌트를 가지고 있는 GameObject를 찾는다.
+                GameObject smObj = GameObject.Find("Score Manager");
+                // 0-2. SetScore()를 가지고 있는 ScoreManager 컴포넌트를 찾는다.
+                ScoreManager sm = smObj.GetComponent<ScoreManager>();
+                // 0-1. SetScore 실행 // 0. 현재 점수 +1 증가
+                sm.SetScore();
             }
         }
         if (collision.gameObject.name.Equals("Player"))
-        {
-            //Debug.Log("collision");
-            Destroy(collision.gameObject);
+        {            
+            //Destroy(collision.gameObject);
             Destroy(gameObject);   
         }
     }
---
+++ Scripts/ScoreManager.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreManager : MonoBehaviour
+{
+    //역할 : 점수를 관리
+    //A : Enemy 잡을 때 마다 점수 획득, 화면에 표시
+    // 화면 표시 text, 점수
+    public TextMeshProUGUI currentScoreText;
+    public int currentScore = 0;
+
+    //B : 현재 점수가 저장된 최고 점수를 넘겼다면 갱신하여 표시
+    // 화면 표시 text, 최고 점수
+
+    //C : 최고 점수를 저장/불러오기를 통해 관리한다.
+    //Save, Load
+    public TextMeshProUGUI bestScoreText;
+     int bestScore = 10;
+    void Start()
+    {
+        // 2. 증가 시킨 점수를 화면에 표시
+        currentScoreText.text = "Score : " + currentScore;
+        //2-1. 최고 점수 불러온다(Load)
+        bestScore = PlayerPrefs.GetInt("@Best_Score", 0);
+        //2-2. 최고 점수를 화면에 표시한다.
+        bestScoreText.text = "BestScore : " + bestScore;
+        
+    }
+
+  
+    void Update()
+    {
+        
+    }
+    
+    public void SetScore()
+    {
+        // 1. 점수를 증가시킨다 (+1)
+        currentScore += 1;
+        // 1-1. currentScore = 현재점수 +1
+        // 2. 증가 시킨 점수를 화면에 표시
+        currentScoreText.text = "Score : " + currentScore;
+        if (currentScore > bestScore) //3. 만약 현재 점수가 최고 점수를 넘어선다면?
+        {
+            bestScore = currentScore; //4. 저장된 최고 점수를 현재점수로 갱신한다.
+                                      //2-2. 최고 점수를 화면에 표시한다.
+            bestScoreText.text = "BestScore : " + bestScore;
+            //6. 최고 점수를 갱신하면 Save
+            PlayerPrefs.SetInt("@Best_Score", bestScore);
+        }
+       
+    }
+    
+}
